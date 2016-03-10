4f9e601a5ff0526bc1a123494ff38aa2b13927aa
Sean Muck <SeanMuck@shelterinsurance.com>
2016-03-10 10:13:00 -0600

    add carousel

--- blog/models.py
+++ blog/models.py
@@ -9,13 +9,73 @@ from wagtail.wagtailadmin.edit_handlers import (FieldPanel,
                                                 MultiFieldPanel,
                                                 PageChooserPanel)
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtaildocs.edit_handlers import DocumentChooserPanel
 from wagtail.wagtailsearch import index
+from wagtail.wagtailimages.blocks import ImageChooserBlock
+from wagtail.wagtaildocs.blocks import DocumentChooserBlock
 
 from modelcluster.tags import ClusterTaggableManager
 from taggit.models import TaggedItemBase
 
 class BlogPageTag(TaggedItemBase):
     content_object = ParentalKey('blog.BlogPage', related_name='tagged_items')
+    
+class LinkFields(models.Model):
+    link_external = models.URLField("External link", blank=True)
+    link_page = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+    link_document = models.ForeignKey(
+        'wagtaildocs.Document',
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+
+    @property
+    def link(self):
+        if self.link_page:
+            return self.link_page.url
+        elif self.link_document:
+            return self.link_document.url
+        else:
+            return self.link_external
+
+    panels = [
+        FieldPanel('link_external'),
+        PageChooserPanel('link_page'),
+        DocumentChooserPanel('link_document'),
+    ]
+
+    class Meta:
+        abstract = True
+    
+class CarouselItem(LinkFields):
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    embed_url = models.URLField("Embed URL", blank=True)
+    caption = models.CharField(max_length=255, blank=True)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('embed_url'),
+        FieldPanel('caption'),
+        MultiFieldPanel(LinkFields.panels, "Link"),
+    ]
+
+    class Meta:
+        abstract = True
+        
+class BlogPageCarouselItem(Orderable, CarouselItem):
+    page = ParentalKey('blog.BlogPage', related_name='carousel_items')
 
 class BlogPage(Page):
     main_image = models.ForeignKey(
@@ -47,6 +107,7 @@ class BlogPage(Page):
         ImageChooserPanel('main_image'),
         FieldPanel('intro'),
         FieldPanel('body'),
+        InlinePanel('carousel_items', label="Carousel items"),
     ]
     
     promote_panels = Page.promote_panels + [
