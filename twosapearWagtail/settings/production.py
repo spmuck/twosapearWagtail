9b92557b45ceabc45cbe108910607b35091c45ce
Sean Muck <spmuck@gmail.com>
2016-03-13 15:18:30 -0500

    styles

--- twosapearWagtail/settings/production.py
+++ twosapearWagtail/settings/production.py
@@ -6,13 +6,6 @@ DEBUG = False
 SECRET_KEY = '0&-k88e%rjgqdy(#h_nr=sdf&)w&l2p7my+5t4i5s(-!vv#1('
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/sean/twosapearWagtail/db.sqlite3'
-    }
-}
-
 try:
     from .local import *
 except ImportError:
