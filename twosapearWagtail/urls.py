cb529ad023aac6eb52da33d620930fb44ae204d7
Sean Muck <spmuck@gmail.com>
2016-03-20 12:24:26 -0500

    more debug tests

--- twosapearWagtail/urls.py
+++ twosapearWagtail/urls.py
@@ -21,11 +21,10 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
-    # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve static and media files from development server
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
