c5cedde9e0967918667efe69f89f13b5a9b7d476
Sean Muck <spmuck@gmail.com>
2016-03-20 12:17:43 -0500

    production settings

--- twosapearWagtail/settings/production.py
+++ twosapearWagtail/settings/production.py
@@ -5,6 +5,7 @@ DEBUG = False
 
 SECRET_KEY = '0&-k88e%rjgqdy(#h_nr=sdf&)w&l2p7my+5t4i5s(-!vv#1('
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'twosapear.com']
 
 try:
     from .local import *
