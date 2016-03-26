78d6bfad0f47c01fc5ad0fd1c19b004c560e1477
Sean Muck <spmuck@gmail.com>
2016-03-26 11:22:02 -0500

    facebook social fields

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -10,9 +10,19 @@ def social_meta(context, calling_page=None):
         feed_image = calling_page.feed_image
     elif calling_page.main_image:
         feed_image = calling_page.main_image
+    if calling_page.feed_title:
+        feed_title = calling_page.feed_title
+    elif calling_page.title:
+        feed_title = calling_page.title
+    if calling_page.feed_description:
+        feed_description = calling_page.feed_description
+    elif calling_page.intro:
+        feed_description = calling_page.intro
     return {
         'calling_page': calling_page,
         'feed_image' : feed_image,
+        'feed_title' : feed_title,
+        'feed_description' : feed_description,
         # required by the pageurl tag that we want to use within this template
         'request': context['request'],
     }
