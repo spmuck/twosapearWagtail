eea110fde2519206fdb8fb5e677cef55f43fb1c1
Sean Muck <spmuck@gmail.com>
2016-03-26 13:08:38 -0500

    refactoring and pinterest linkin

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -27,6 +27,29 @@ def social_meta(context, calling_page=None):
         'request': context['request'],
     }
 
+@register.inclusion_tag('blog/tags/social_drop_down.html', takes_context=True)
+def social_drop_down(context, calling_page=None):
+    if calling_page.feed_image:
+        feed_image = calling_page.feed_image
+    elif calling_page.main_image:
+        feed_image = calling_page.main_image
+    if calling_page.feed_title:
+        feed_title = calling_page.feed_title
+    elif calling_page.title:
+        feed_title = calling_page.title
+    if calling_page.feed_description:
+        feed_description = calling_page.feed_description
+    elif calling_page.intro:
+        feed_description = calling_page.intro
+    return {
+        'page': calling_page,
+        'feed_image' : feed_image,
+        'feed_title' : feed_title,
+        'feed_description' : feed_description,
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
+
 @register.assignment_tag()    
 def get_domain():
     return settings.DOMAIN
