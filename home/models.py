3c18911f47443a9227e888238161b3dbfc2087ae
Sean Muck <spmuck@gmail.com>
2016-04-16 14:44:45 -0500

    add paginator to home page #8

--- home/models.py
+++ home/models.py
@@ -1,12 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from blog.models import BlogPage
 from taggit.models import Tag
+
 
 
 class HomePage(Page):
@@ -18,5 +20,17 @@ class HomePage(Page):
     
     def get_context(self, request):
         context = super(HomePage, self).get_context(request)
-        context['posts'] = BlogPage.objects.live().order_by('-date')[:5]
+        posts = BlogPage.objects.live().order_by('-date')
+        page = request.GET.get('page', 1)
+        
+        paginator = Paginator(posts, 5)
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
+        
+        context['posts'] = posts;
+        
         return context
